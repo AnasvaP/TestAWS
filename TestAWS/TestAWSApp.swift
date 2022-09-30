81fcd79c14b45f05c09c29ae1fa7c088241cf0cd
AnasvaP <anasvap@icloud.com>
2022-09-30 12:20:55 +0300

    Initial Commit

---
+++ TestAWS/TestAWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAWSApp.swift
+//  TestAWS
+//
+//  Created by Anastasiia Poliuchovych on 30.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestAWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
